dbac1c61cdce148709db450a9ae1d678188a8df9
Nha Le Truc <nha.letruc@gmail.com>
2025-04-14 13:54:27 +0700

    [Terraform] Modifies resources for etcd, balancers, pgbackrest

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -1,52 +1,7 @@
 ############################################
-## etcd_cluster
+## distributed key-value stores
 ############################################
-resource "proxmox_virtual_environment_vm" "etcd" {
-  count = lookup(var.node_count, "workers", 0)
-  name  = "cluster-worker-${count.index}"
-  description = "Managed by Terraform"
-  tags = ["terraform", "ubuntu", "worker"]
-
-  node_name = "pve"
-  vm_id = var.new_vm_id + count.index
-
-  clone {
-    vm_id = var.template_id
-    full = true
-    retries = 3
-  }
-
-  agent {
-    enabled = true
-  }
-
-  # if agent is not enabled, the VM may not be able to shutdown properly, and may need to be forced off
-  stop_on_destroy = true
-
-  cpu {
-    cores = 2
-    type = "x86-64-v2-AES"  # recommended for modern CPUs
-  }
-
-  memory {
-    dedicated = 4096
-    floating  = 4096 # set equal to dedicated to enable ballooning
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-  }
-
-}
-
-############################################
-## consul_instances
-############################################
-resource "proxmox_virtual_environment_vm" "consul" {
+resource "proxmox_virtual_environment_vm" "kvs" {
   count = lookup(var.node_count, "workers", 0)
   name  = "cluster-worker-${count.index}"
   description = "Managed by Terraform"
@@ -134,7 +89,7 @@ resource "proxmox_virtual_environment_vm" "balancers" {
 }
 
 ############################################
-## pgbackrest
+##  backup and restore
 ############################################
 resource "proxmox_virtual_environment_vm" "pgbackrest" {
   count = lookup(var.node_count, "workers", 0)
@@ -280,16 +235,16 @@ resource "local_file" "tf_ansible_inventory_file" {
 # if dcs_exists: false and dcs_type: "etcd"
 [etcd_cluster]  # recommendation: 3, or 5-7 nodes
 #10.128.64.140
-${join("\n", [for ip in proxmox_virtual_environment_vm.workers.*.ipv4_addresses : join(",", ip[1])])}
+${join("\n", [for ip in proxmox_virtual_environment_vm.kvs.*.ipv4_addresses : join(",", ip[1])])}
 
 # if dcs_exists: false and dcs_type: "consul"
 [consul_instances]  # recommendation: 3 or 5-7 nodes
 #10.128.64.140 consul_node_role=server consul_bootstrap_expect=true consul_datacenter=dc1
-%{for ip in [for ips in proxmox_virtual_environment_vm.workers.*.ipv4_addresses : join(",", ips[1])]~}
+%{for ip in [for ips in proxmox_virtual_environment_vm.kvs.*.ipv4_addresses : join(",", ips[1])]~}
 ${ip} consul_node_role=server consul_bootstrap_expect=true consul_datacenter=dc1
 %{endfor~}
 #10.128.64.144 consul_node_role=client consul_datacenter=dc2
-%{for ip in [for ips in proxmox_virtual_environment_vm.workers.*.ipv4_addresses : join(",", ips[1])]~}
+%{for ip in [for ips in proxmox_virtual_environment_vm.kvs.*.ipv4_addresses : join(",", ips[1])]~}
 ${ip} consul_node_role=client consul_datacenter=dc2
 %{endfor~}
 
@@ -297,7 +252,7 @@ ${ip} consul_node_role=client consul_datacenter=dc2
 [balancers]
 #10.128.64.140 # balancer_tags="datacenter=dc1"
 #10.128.64.145 # balancer_tags="datacenter=dc2" new_node=true
-${join("\n", [for ip in proxmox_virtual_environment_vm.workers.*.ipv4_addresses : join(",", ip[1])])}
+${join("\n", [for ip in proxmox_virtual_environment_vm.balancers.*.ipv4_addresses : join(",", ip[1])])}
 
 # PostgreSQL nodes
 [master]
@@ -327,6 +282,7 @@ replica
 # if pgbackrest_install: true and "repo_host" is set
 [pgbackrest]  # optional (Dedicated Repository Host)
 #10.128.64.110
+${join("\n", [for ip in proxmox_virtual_environment_vm.pgbackrest.*.ipv4_addresses : join(",", ip[1])])}
 
 [pgbackrest:vars]
 #ansible_user='postgres'
